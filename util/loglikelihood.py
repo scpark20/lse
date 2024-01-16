43f9396113759380a5653a261e9fec615514c8da
soochul park <scpark20@gmail.com>
2024-01-16 17:15:44 +0900

    CNLL Test

---
+++ util/loglikelihood.py
@@ -0,0 +1,28 @@
+import torch
+import numpy as np
+
+def get_nll(p_samples, q_samples, log_sigma):
+    # p_samples : (M, z)
+    # q_samples : (N, z)
+    M = len(p_samples)
+    N = len(q_samples)
+    z_dim = p_samples.shape[1]
+    
+    # (N, M)
+    distance = torch.norm(q_samples.unsqueeze(1) - p_samples.unsqueeze(0), dim=2) ** 2
+    alpha = -1/(2*np.exp(log_sigma)**2)
+    nll = -torch.mean(torch.logsumexp(alpha*distance, dim=0))
+    nll = nll + 0.5*z_dim*(2*log_sigma-np.log(np.e)) + np.log(N)
+    
+    return nll.item()
+
+def get_optimum_log_sigma(p_samples1, p_samples2, min_log_sigma=-3, max_log_sigma=3):
+    log_sigmas = np.linspace(min_log_sigma, max_log_sigma, 100)
+    nlls = np.array([get_nll(p_samples1, p_samples2, log_sigma) for log_sigma in log_sigmas])
+    return log_sigmas[np.argmin(nlls)]
+
+def get_cross_nll(p_samples, q_samples, log_sigma):
+    nll_p2q = get_nll(p_samples, q_samples, log_sigma)
+    nll_q2p = get_nll(q_samples, p_samples, log_sigma)
+    return (nll_p2q + nll_q2p) / 2
+    
---
+++ util/spiral.py
@@ -0,0 +1,24 @@
+import torch
+import numpy as np
+
+def generate_spiral_data_torch(points_per_class, num_classes):
+    """
+    This function generates spiral data with the specified number of points per class
+    and the specified number of classes using PyTorch.
+    """
+    X = []
+    y = []
+    num_points = points_per_class * num_classes
+    radius = torch.linspace(0.0, 1, points_per_class)
+    
+    for i in range(num_classes):
+        theta = torch.linspace(i * 4 * np.pi / num_classes, (i + 1) * 4 * np.pi / num_classes, points_per_class) + torch.randn(points_per_class) * 0.2
+        x1 = radius * torch.sin(theta)
+        x2 = radius * torch.cos(theta)
+        X.append(torch.stack((x1, x2), dim=1))
+        y += [i] * points_per_class
+
+    X = torch.cat(X)
+    y = torch.tensor(y)
+    return X, y
+        
